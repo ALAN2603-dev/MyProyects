ab6736fa378bb8f2d9e2ca81651fa71a4e678741
ALAN2603-dev <alanjaramillogomez90@gmail.com>
2026-09-19 20:11:17 -0700

    Add files via upload

---
+++ PRACTICA 2 SEMAFORO.c
@@ -0,0 +1,42 @@
+#include <16f873.h>
+#fuses xt, nolvp, nowdt
+#use  delay(clock=4000000)
+#byte porta=5
+#byte portb=6
+#byte portc=7
+
+void main()
+{
+int x=0, y=0;
+   set_tris_a(0xff);//input
+   set_tris_b(0x00);//output
+
+      while(true)
+      {
+         if(input(pin_a0))
+         {
+         portb=0x11;
+         delay_ms(10000);
+         }
+            else
+            {
+            portb=0x0c;
+            delay_ms(7000);
+            portb=0x0a;
+            delay_ms(500);
+            portb=0x08;
+            delay_ms(500);
+            portb=0x0a;
+            delay_ms(500);
+            portb=0x08;
+            delay_ms(500);
+            portb=0x0a;
+            delay_ms(500);
+            portb=0x08;
+            delay_ms(500);
+            portb=0x09;
+            delay_ms(10000);
+            }
+      }
+}
+
